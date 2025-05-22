4d0e68e02fda7f6cd84bc044b536d7be2f94b7c8
Vlad Niculescu-Bolintin <vlad.nic@gmail.com>
2025-05-22 11:59:44 +0100

    fix

--- LINQPadPlus/HTML/Utils/HtmlBeautifier.cs
+++ LINQPadPlus/HTML/Utils/HtmlBeautifier.cs
@@ -5,7 +5,23 @@ namespace LINQPadPlus;
 
 public static class HtmlBeautifier
 {
-	public static string BeautifyHtml(this string s)
+	public static string BeautifyHtml(this string s) =>
+		s.Trim().StartsWith("<!DOCTYPE html>", StringComparison.OrdinalIgnoreCase) switch
+		{
+			true => s.BeautifyHtmlFull(),
+			false => s.BeautifyHtmlFrag(),
+		};
+
+	static string BeautifyHtmlFull(this string s)
+	{
+		var parser = new HtmlParser();
+		var document = parser.ParseDocument(s);
+		using var sw = new StringWriter();
+		document.ToHtml(sw, new PrettyMarkupFormatter());
+		return sw.ToString().Trim();
+	}
+
+	static string BeautifyHtmlFrag(this string s)
 	{
 		var parser = new HtmlParser();
 		var dom = parser.ParseDocument("<html><body></body></html>");
