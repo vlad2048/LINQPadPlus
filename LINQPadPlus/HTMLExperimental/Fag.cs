0cfb7cea69fd695473a82cb3cf950719eee28838
Vlad Niculescu-Bolintin <vlad.nic@gmail.com>
2025-06-24 03:02:20 +0100

    added ToTag()

---
+++ LINQPadPlus/HTMLExperimental/Fag.cs
@@ -0,0 +1,217 @@
+﻿/*
+using JetBrains.Annotations;
+using LINQPad;
+using LINQPadPlus._sys.TagUtils;
+using LINQPadPlus._sys.Utils;
+using LINQPadPlus._sys;
+using System.Diagnostics.CodeAnalysis;
+using System.Reactive;
+using System.Runtime.CompilerServices;
+using System.Text;
+using LINQPad.Controls;
+using LINQPadPlus.Rx;
+
+namespace LINQPadPlus;
+
+public class Fag
+{
+	readonly string name;
+	readonly Dictionary<string, JSVal?> attrs = new();
+	readonly HashSet<string> classes = [];
+	readonly List<string> styles = [];
+	readonly List<object> kids = [];
+	readonly ISig whenPreRender = new Sig();
+	readonly ISig whenPostRender = new SigAsync();
+
+	public IObservable<Unit> WhenPostRender => whenPostRender;
+
+	[field: AllowNull, MaybeNull]
+	public string Id
+	{
+		get => field ??= IdGen.Make();
+		set
+		{
+			if (field != null) throw new InvalidOperationException("Id can only be set once. You neet to call id() before listen() or Run()");
+			field = value;
+		}
+	}
+
+
+	protected internal Fag(string name) => this.name = name;
+
+
+	public object ToDump() => Util.RawHtml(RenderString(true));
+
+	public override string ToString() => RenderString(false).BeautifyHtml();
+
+	internal string RenderString(bool runJs)
+	{
+		if (runJs)
+		{
+			var readyDispatchId = $"{Id}-OnReady";
+			Events.Listen(readyDispatchId, whenPostRender.Trig);
+			this.Run("_ => dispatch(____0____, {})", e => e.JSRepl_Val(0, readyDispatchId));
+
+			whenPreRender.Trig();
+		}
+
+		var sb = new StringBuilder();
+		sb.WriteTagOpenStart(name);
+		sb.WriteId(Id);
+		sb.WriteClasses(classes);
+		foreach (var (key, val) in attrs.Where(t => t.Value != null))
+			val!.WriteAttribute(sb, key);
+		sb.WriteStyles(styles);
+		sb.WriteTagOpenEnd();
+
+		foreach (var kid in kids)
+		{
+			var kidStr = kid.RenderString(runJs);
+			sb.Append(kidStr);
+		}
+
+		if (!t.VoidElements.Contains(name))
+			sb.WriteTagClose(name);
+
+		return sb.ToString();
+	}
+
+	// @formatter:off
+	public Fag id(string id__) => this.With(() => Id = id__);
+	public Fag cls(string className, bool condition = true) => this.With(() => classes.Add(className), condition);
+	public Fag style(string style, bool condition = true) => this.With(() => styles.Add(style), condition);
+	public Fag attr(string key, JSVal? value, bool condition = true) => this.With(() => attrs[key] = value, condition);
+	// @formatter:on
+
+	public Fag this[params object[] kids_] => this.With(() =>
+	{
+		if (t.VoidElements.Contains(name))
+			throw new ArgumentException($"<{name}> elements cannot have children");
+		kids.AddRange(kids_);
+	});
+
+	public Fag js(
+		string onRender,
+		[CallerMemberName] string? srcMember = null,
+		[CallerFilePath] string? srcFile = null,
+		[CallerLineNumber] int srcLine = 0
+	) =>
+		// ReSharper disable ExplicitCallerInfoArgument
+		this.With(() => whenPostRender.Subscribe(_ => this.Run(onRender, null, srcMember, srcFile, srcLine)));
+	// ReSharper restore ExplicitCallerInfoArgument
+
+	public Fag On(string evtName, string[] evtProps, Action<IDictionary<string, string>> action) =>
+		this.With(() =>
+			whenPostRender.Subscribe(_ =>
+			{
+				var ctrl = new Control(name)
+				{
+					HtmlElement =
+					{
+						ID = Id,
+					},
+					CssClass = JSRunIdentifiers.ClassToRemove,
+				};
+				ctrl.HtmlElement.AddEventListener(evtName, evtProps, (_, args) => action(args.Properties));
+				ctrl.Styles["display"] = "none";
+				ctrl.Dump();
+			})
+		);
+
+	public Fag On(string evtName, Action action) =>
+		this.With(() =>
+			whenPostRender.Subscribe(_ =>
+			{
+				var ctrl = new Control(name)
+				{
+					HtmlElement =
+					{
+						ID = Id,
+					},
+					CssClass = JSRunIdentifiers.ClassToRemove,
+				};
+				ctrl.HtmlElement.AddEventListener(evtName, (_, _) => action());
+				ctrl.Styles["display"] = "none";
+				ctrl.Dump();
+			})
+		);
+
+	public Fag onReady(Action onReady) => this.With(() => whenPostRender.Subscribe(_ => onReady()));
+}
+
+
+
+
+public static class FagUtils
+{
+	public static Fag listen(this Fag tag, string eventName, Action action, bool condition = true) => tag.listen(eventName, null, _ => action(), condition);
+
+
+	public static void Run(
+		this Fag tag,
+		[LanguageInjection(InjectedLanguage.JAVASCRIPT)] string codeFun,
+		Func<string, string>? replFun = null,
+		[CallerMemberName] string? srcMember = null,
+		[CallerFilePath] string? srcFile = null,
+		[CallerLineNumber] int srcLine = 0
+	) =>
+		JS.Run(
+			"""
+			(async () => {
+				try {
+					const elt = await window.waitForElement(____998____);
+					(____999____)(elt);
+				} catch (err) {
+					dispatchError(err, runId);
+				}
+			})();
+			""",
+			e =>
+				JS.Fmt(
+					e
+						.JSRepl_Val(998, tag.Id)
+						.JSRepl_Var(999, codeFun),
+					replFun
+				),
+			srcMember,
+			// ReSharper disable ExplicitCallerInfoArgument
+			srcFile,
+			srcLine
+		// ReSharper restore ExplicitCallerInfoArgument
+		);
+}
+
+
+
+
+
+
+file static class FagWriter
+{
+	public static void WriteTagOpenStart(this StringBuilder sb, string name) => sb.Append($"<{name}");
+
+	public static void WriteId(this StringBuilder sb, string id) => sb.Append($" id='{id}'");
+
+	public static void WriteClasses(this StringBuilder sb, HashSet<string> classes)
+	{
+		if (classes.Count == 0) return;
+		sb.Append($" class='{classes.JoinText(" ")}'");
+	}
+	public static void WriteStyles(this StringBuilder sb, List<string> styles)
+	{
+		if (styles.Count == 0) return;
+		var stylesStr = styles.Select(e => e.Trim().RemoveSuffixIFN(";")).JoinText("; ");
+		sb.Append($" style='{stylesStr}'");
+	}
+	public static void WriteTagOpenEnd(this StringBuilder sb) => sb.Append(">");
+
+	public static void WriteTagClose(this StringBuilder sb, string name) => sb.Append($"</{name}>");
+
+	static string RemoveSuffixIFN(this string s, string suffix) =>
+		s.EndsWith(suffix) switch
+		{
+			true => s[..^suffix.Length],
+			false => s,
+		};
+}
+*/
